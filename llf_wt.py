de531d87e40da24f79f15eda422050c327b9e327
Ashish Kumar <35109474+bashish2711@users.noreply.github.com>
2018-05-17 01:50:04 +0000

    Update llf_wt.py

--- llf_wt.py
+++ llf_wt.py
@@ -1,3 +1,45 @@
+#!/usr/bin/python
+
+# Import modules for CGI handling 
+import cgi, cgitb
+import os
+print "Content-type:text/html\r\n\r\n"
+print """
+<html>
+   <head> </head>
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+	<style>
+	table {
+    border-collapse: collapse;
+    border-spacing: 0;
+    width: 100%;
+    border: 1px solid #ddd;
+		}
+
+	th, td {
+    text-align: left;
+    padding: 8px;
+		}
+
+	tr:nth-child(even){background-color: #f2f2f2}
+	</style>
+   <title> Sucess </title>
+   <body>
+   <h2> The code Executed Successfully </h2>
+   <a href=/html_show.html target=_new> HTML Gantt Chart </a>
+   <a href=/out_show.txt target=_new> Text Log File </a>
+   <p> Gantt Chart </p>
+   <iframe name="Iframe2" src="/html_show.html" width=100% height=200px> </iframe>
+   <p> Run Log </p>
+   <iframe name="Iframe1" src="/out_show.txt" width=100% height=200px > </iframe> 
+   
+   </body>
+</html>
+"""
+
+out = "Content-type:text/html\r\n\r\n"
+out += "RunTime\tName\tArrival\tBT\tStart\tUSE\tPRI\tEND\tSTATUS \n"
+
 #Least laxity first scheduling implementation
 
 import string
@@ -13,30 +55,39 @@ def lcm(a):
 class TaskIns(object):
 
      #Constructor (should only be invoked with keyword parameters)
-    def __init__(self, start, end, priority, name, bt):
-        self.start    = start
+    def __init__(self, at, end, priority, name, bt):
+        self.at    = at
         self.end      = end
         self.usage    = 0
         self.priority = priority
         self.name     = name.replace("\n", "")
-        self.id = int(random.random() * 10000)
+        self.id = int(random.random() * 100)
         self.bt = bt
+        self.run_time = 0
+        self.start = at
+        self.finish = 0
 
     #Allow an instance to use the cpu (periodic)
     def use(self, usage):
+        global out, run_time
+        self.run_time = run_time+clock_step-1
         self.usage += usage
+        self.start = self.run_time
+        self.finish = self.start + clock_step
         if self.usage >= self.bt:
             self.status = "Finish"
         else:
             self.status = " "
-        print on_cpu.get_unique_name() + " " + "\t %s \t %s \t %s \t %s \t %s \t %s \t %s" %(self.start, self.bt, self.start, clock_step , self.priority, self.end, self.status)    
-        if self.usage >= self.end - self.start:
+        out += str(self.run_time) + "\t" + str(on_cpu.name) +"\t"+ str(self.at)+"\t" + str(self.bt)+"\t"  + str(self.start) +"\t"+ str(clock_step) +"\t"+ str(self.priority) +"\t"+ str(self.finish)+"\t"+  str(self.status) 
+        if self.usage >= self.end - self.at:
             return True
+            self.start=0
         return False
+        self.start=0
     
     #Default representation
     #def __repr__(self):
-    #    return str(self.name) + "#" + str(self.id) + " - start: " + str(self.start) + " priority: " + str(self.priority) + budget_text
+    #    return str(self.name) + "#" + str(self.id) + " - at: " + str(self.at) + " priority: " + str(self.priority) + budget_text
 
     #Get name as Name#id
     def get_unique_name(self):
@@ -92,7 +143,6 @@ if __name__ == '__main__':
             raise Exception('Invalid tasks.txt file structure')
         if int(line[0])>=0:
             task_types.append(TaskType(arrival_time=line[0], burst_time=line[1], period=line[2], deadline=line[3], name=name))
-        
     #Calculate hyperperiod
     for task_type in task_types:
         hyperperiod.append(task_type.period)
@@ -106,62 +156,96 @@ if __name__ == '__main__':
     for i in xrange(0, hyperperiod):
         for task_type in task_types:
             if  (i - task_type.arrival_time) % task_type.period == 0 and i >= task_type.arrival_time:
-                start = i
-                end = start + task_type.burst_time
+                at = i
+                end = at + task_type.burst_time
                 priority = task_type.deadline - task_type.burst_time -task_type.arrival_time
-                tasks.append(TaskIns(start=start, end=end, priority=priority, name=task_type.name, bt=task_type.burst_time))
+                tasks.append(TaskIns(at=at, end=end, priority=priority, name=task_type.name, bt=task_type.burst_time))
 
-    #Html output start
-    html = "<!DOCTYPE html><html><head><title>LLF Scheduling</title></head><body>"
+    #Html output at
+    html = "Content-type:text/html\r\n\r\n"
+    html = """
+    <!DOCTYPE html><html><head><title>LLF Scheduling</title></head><body>
+           <meta name="viewport" content="width=device-width, initial-scale=1">
+	<style>
+	table {
+    border-collapse: collapse;
+    border-spacing: 0;
+    width: 100%;
+    border: 1px solid #ddd;
+		}
 
+	th, td {
+    text-align: left;
+    padding: 8px;
+		}
+
+	tr:nth-child(even){background-color: #f2f2f2}
+	</style>
+   <title> Sucess </title>
+   		<div style="overflow-x:auto">
+		<table> <tr>
+
+		"""
     #Check utilization
     utilization = 0
     for task_type in task_types:
         utilization += float(task_type.burst_time) / float(task_type.period)
     if utilization > 1:
-        print 'Utilization error!'
+        out += 'Utilization error!'
         html += '<br /><br />Utilization error!<br /><br />'
 
     #Simulate clock
     clock_step = 1
+    run_time = 0
     for i in xrange(0, hyperperiod, clock_step):
         #Fetch ready_list tasks that can use cpu and sort by priority
         ready_list = []
         for t in tasks:
-            if t.start <= i:
+            if t.at <= i:
                 ready_list.append(t)
         ready_list = sorted(ready_list, priority_cmp)
 
         #Select task with highest priority
+        # Print Tasks
         if len(ready_list) > 0:
             on_cpu = ready_list[0]
             done_list.append(on_cpu)
-            html += '<div style="float: left; text-align: center; width: 110px; height: 20px; background-color:' + html_color[on_cpu.name] + ';">' + on_cpu.get_unique_name() + '</div>'
+            html += '<td style="background-color:' + html_color[on_cpu.name] + ';">' + on_cpu.get_unique_name() + '</td>' + '\n'
             on_cpu.priority += clock_step
             if on_cpu.use(clock_step):
                 tasks.remove(on_cpu)
-                html += '<div style="float: left; text-align: center; width: 10px; height: 20px; background-color:' + html_color['Finish'] + ';"></div>'
-               # print "Finish!" ,
-          #  else:
-			#	print "TBD"
+                run_time += clock_step
+                # Not Print Finish as it is showing on Gantt chart
+                #html += '<td style="text-align: center; width: 10px; height: 50px; background-color:' + html_color['Finish'] + ';"></td>'  + '\n'
+               # out += "Finish!" ,
+            else:
+            	# Processor is not used but runtime is going
+            	run_time += clock_step
         else:
-            print 'No task uses the processor. '
-            html += '<div style="float: left; text-align: center; width: 110px; height: 20px; background-color:' + html_color['Empty'] + ';">Empty</div>'
-        print "\n"
+            out += 'No task uses the processor. '
+            # Processor is not used but runtime is going
+            run_time += clock_step
+            html += '<td style=" text-align: center; width: 20px; height: 50px; background-color:' + html_color['Empty'] + ';">E</td>'  + '\n'
+        out += "\n"
             
 
-    #Print remaining periodic tasks
+    #out += remaining periodic tasks
     html += "<br /><br />"
     for p in tasks:
-        print p.get_unique_name() + " is dropped due to overload!"
+        out += p.get_unique_name() + " is dropped due to overload!"
         html += "<p>" + p.get_unique_name() + " is dropped due to overload!</p>"
     
-        
-    # Print Waiting time and turnaround time calculations:
+    #Table done, print period below table
+    html += "</tr>"
+    # at New row to print runtime
+    html += "<tr>"
+    for run_time in range(0, hyperperiod+1) :
+    	html +='<td>' + str(run_time) + '</td>'
+       
+    # out += Waiting time and turnaround time calculations:
     b_t=[]
     processes=[]
-    for d in range(0, 25):
-    	print done_list[d].name, done_list[d+1].bt, "\n"
+    for d in range(1, len(list(done_list))):
     	b_t.append(done_list[d].bt)
     	processes.append(done_list[d].name)
     
@@ -179,16 +263,18 @@ if __name__ == '__main__':
     	avgtat+=tat[i]
     	avgwt=float(avgwt)/n
     	avgtat=float(avgtat)/n
-    print("\n")
-    print("Process\t  Burst Time\t  Waiting Time\t  Turn Around Time")
+    out += "\n"
+    out += "Process\t  Burst Time\t  Waiting Time\t  Turn Around Time \n"
     for i in range(0,n):
-    	print(str(processes[i])+"\t\t"+str(b_t[i])+"\t\t"+str(wt[i])+"\t\t"+str(tat[i]))
-    	print("\n")
-    print("Average Waiting time is: "+str(avgwt))
-    print("Average Turn Arount Time is: "+str(avgtat))
-
+    	out += str(processes[i])+"\t\t"+str(b_t[i])+"\t\t"+str(wt[i])+"\t\t"+str(tat[i])
+    	out += "\n" 
+    out += "Average Waiting time is: "+str(avgwt) + "\n"
+    out += "Average Turn Arount Time is: "+str(avgtat) + "\n"
     #Html output end
     html += "</body></html>"
-    output = open('output.html', 'w')
-    output.write(html)
-    output.close()
+    html_show = open('../pi/html_show.html', 'w')
+    html_show.write(html)
+    html_show.close()
+    out_show = open('../pi/out_show.txt', 'w')
+    out_show.write(out)
+    out_show.close()
